82fc7667839f11a1948fbeba0bffbe35b557ae38
Josh Freed <josh@jpfreed.com>
2021-11-27 09:44:36 -0500

    Initial Commit

---
+++ MedTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedTracker
+//
+//  Created by Josh Freed on 11/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedTracker/MedTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedTrackerApp.swift
+//  MedTracker
+//
+//  Created by Josh Freed on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MedTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
